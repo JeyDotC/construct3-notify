0194278575995c71e498004b8dd16313d1334748
Jeysson Guevara <guevara@civicplus.com>
2021-07-22 18:44:35 -0500

    WIP: first working version.

--- src/c3runtime/actions.js
+++ src/c3runtime/actions.js
@@ -3,38 +3,37 @@
 {
 	self.C3.Behaviors.JeyDotC_Notify.Acts =
 	{
-		Notify(event)
-		{
-			const {debounced, debouncedFor, debounceStart} = this._notifyMap[event] || { debounced: false };
+		Notify(event, notifyAfter = 0) {
+			const existingEvent = this._notifyMap[event];
+			const eventExists = existingEvent !== undefined;
 
-			if(debounced){
-				const ellapsedTime = Date.now() - debounceStart;
-				if(ellapsedTime < debouncedFor * 1000){
-					return;
-				}	
+			if (!eventExists) {
+				this._notifyMap[event] = {
+					notified: false,
+					debounced: false,
+					notifyAfter,
+					notifyCreated: Date.now()
+				};
 			}
-
-			this._notifyMap[event] = {
-				notified: true,
-				debounced: false
-			};
-			this.Trigger(C3.Behaviors.JeyDotC_Notify.Cnds.OnNotified);
 		},
 
-		DebounceFor(event, seconds){
-			// Avoid accumulated debouncing.
-			if(this._notifyMap[event] && this._notifyMap[event].debounced){
-				return;
+		DebounceFor(event, debouncedFor) {
+			const existingEvent = this._notifyMap[event];
+			const eventExists = existingEvent !== undefined;
+
+			if (!eventExists) {
+				this._notifyMap[event] = {
+					notified: false,
+					debounced: true,
+					debouncedFor,
+					debounceStart: Date.now()
+				};
+			} else if(!existingEvent.debounced) {
+				this._notifyMap[event] = {...existingEvent, debounced: true, debouncedFor, debounceStart: Date.now()}
 			}
-			this._notifyMap[event] = {
-				notified: false,
-				debounced: true,
-				debouncedFor: seconds,
-				debounceStart: Date.now()
-			};
 		},
 
-		Observe(instanceVar, frequency, ignoreDeferred){
+		Observe(instanceVar, frequency, ignoreDeferred) {
 			const currentValue = this.GetObjectInstance().GetInstanceVariableValue(instanceVar);
 			this._watchedVariables[instanceVar] = {
 				frequency,
@@ -45,7 +44,7 @@
 			};
 		},
 
-		StopObserving(instanceVar){
+		StopObserving(instanceVar) {
 			this._watchedVariables.hasOwnProperty(instanceVar) && delete this._watchedVariables[instanceVar];
 		}
 	};
