d8415850772ba0ed307ac24ca0b8ad65b387727b
Jeysson Guevara <guevara@civicplus.com>
2021-08-31 17:16:36 -0500

    Added save/load

--- src/c3runtime/instance.js
+++ src/c3runtime/instance.js
@@ -5,81 +5,75 @@
 
 	const SelfBehavior = C3.Behaviors.JeyDotC_Notify;
 
-	C3.Behaviors.JeyDotC_Notify.Instance = class NotifyInstance extends C3.SDKBehaviorInstanceBase
-	{
-		constructor(behInst, properties)
-		{
+	const applyToAllVarWatches = (allWatches, modifier) => Object.entries(allWatches).map(([instanceVar, watch]) => {
+		return [instanceVar, modifier({ watch, instanceVar })];
+	}).reduce((total, [instanceVar, watch]) => ({ ...total, [instanceVar]: watch }), {});
+
+	C3.Behaviors.JeyDotC_Notify.Instance = class NotifyInstance extends C3.SDKBehaviorInstanceBase {
+		constructor(behInst, properties) {
 			super(behInst);
 
 			this._notifyMap = {};
 
 			this._watchedVariables = {};
-						
+
 			// Opt-in to getting calls to Tick()
 			this._StartTicking();
 			this._StartPostTicking();
 		}
 
-		Release()
-		{
+		Release() {
 			super.Release();
 		}
 
-		_SetMyProperty(n)
-		{
-			this._myProperty = 0;
-		}
 
-		_GetMyProperty()
-		{
-			return this._myProperty;
-		}
-		
-		SaveToJson()
-		{
+		SaveToJson() {
 			return {
-				// data to store for savegames
+				"nmap": this._notifyMap,
+				"wvar": this._watchedVariables
 			};
 		}
 
-		LoadFromJson(o)
-		{
-			// load state for savegames
+		LoadFromJson(o) {
+			const { nmap, wvar } = o;
+
+			this._notifyMap = nmap;
+			this._watchedVariables = applyToAllVarWatches(wvar, ({ watch }) => ({
+				...watch,
+				notified: false,
+				lastNotifiedAt: Date.now(),
+			}));
 		}
-		
-		
-		Tick()
-		{
+
+
+		Tick() {
 			// const dt = this._runtime.GetDt(this._inst);
 			// const wi = this._inst.GetWorldInfo();
 			this.NotifyInstanceVariablesChanged();
 			this.NotifyEvents();
 		}
 
-		PostTick(){
-			this._watchedVariables = Object.entries(this._watchedVariables).map(([instanceVar, watch]) => {
-				return [instanceVar, {
-					...watch, 
-					deferrredChangeDetected: false,
-					notified: false
-				}];
-			}).reduce((total, [instanceVar, watch]) => ({...total, [instanceVar]: watch}), {});
+		PostTick() {
+			this._watchedVariables = applyToAllVarWatches(this._watchedVariables, ({ watch }) => ({
+				...watch,
+				deferrredChangeDetected: false,
+				notified: false
+			}));
 
 			// Discard notified events.
 			this._notifyMap = Object.entries(this._notifyMap)
 				.filter(([, data]) => !data.notified)
-				.reduce((total, [event, data]) => ({...total, [event]: data}), {});
+				.reduce((total, [event, data]) => ({ ...total, [event]: data }), {});
 		}
 
-		GetScriptInterfaceClass()
-		{
+		GetScriptInterfaceClass() {
 			return self.INotifyInstance;
 		}
 
 		NotifyInstanceVariablesChanged() {
-			this._watchedVariables = Object.entries(this._watchedVariables).map(([instanceVar, watch]) => {
-				const {frequency, lastNotifiedAt, currentValue, deferrredChangeDetected, ignoreDeferred} = watch;
-				
+			this._watchedVariables = applyToAllVarWatches(this._watchedVariables, ({ watch, instanceVar }) => {
+				const { frequency, lastNotifiedAt, currentValue, deferrredChangeDetected, ignoreDeferred } = watch;
+
 				let lastNotified = lastNotifiedAt;
 				let deferredChange = deferrredChangeDetected;
 				let notified = false;
@@ -89,29 +83,29 @@
 				const timeElapsedSinceLastWatch = Date.now() - lastNotifiedAt;
 				const matchesFrequency = timeElapsedSinceLastWatch > frequency * 1000;
 
-				if(matchesFrequency && (valueDidChange || deferrredChangeDetected)){
+				if (matchesFrequency && (valueDidChange || deferrredChangeDetected)) {
 					lastNotified = Date.now();
 					deferredChange = false;
 					notified = true;
-				} else if(!matchesFrequency && valueDidChange && !ignoreDeferred){
+				} else if (!matchesFrequency && valueDidChange && !ignoreDeferred) {
 					deferredChange = true;
 				}
 
-				return [instanceVar, {
+				return {
 					frequency,
-					lastNotifiedAt: lastNotified, 
-					currentValue: mostRecentValue, 
+					lastNotifiedAt: lastNotified,
+					currentValue: mostRecentValue,
 					deferrredChangeDetected: deferredChange,
 					notified
-				}];
-			}).reduce((total, [instanceVar, watch]) => ({...total, [instanceVar]: watch}), {});
+				};
+			});
 
 			this.Trigger(SelfBehavior.Cnds.OnInstanceVariableChanged);
 		}
 
-		NotifyEvents(){
+		NotifyEvents() {
 			this._notifyMap = Object.entries(this._notifyMap).map(([event, data]) => {
-				const {debounced, debounceStart, debouncedFor, notifyAfter, notifyCreated} = data;
+				const { debounced, debounceStart, debouncedFor, notifyAfter, notifyCreated } = data;
 
 				const now = Date.now();
 				const deferredNotifyTime = notifyCreated + notifyAfter * 1000;
@@ -121,41 +115,38 @@
 
 				return [event, {
 					notified: shouldNotify,
-					notifyAfter, 
+					notifyAfter,
 					notifyCreated,
 
-					debounced, 
-					debounceStart, 
+					debounced,
+					debounceStart,
 					debouncedFor,
 				}]
-			}).reduce((total, [event, data]) => ({...total, [event]: data}), {});
+			}).reduce((total, [event, data]) => ({ ...total, [event]: data }), {});
 
 			this._notifyMap['DryFire'] !== undefined && console.log(this._notifyMap);
 
 			this.Trigger(SelfBehavior.Cnds.OnNotified);
 		}
 	};
-	
+
 	// Script interface for behavior instance
 	const map = new WeakMap();
-	
+
 	self.INotifyInstance = class INotifyInstance extends self.IBehaviorInstance {
-		constructor()
-		{
+		constructor() {
 			super();
-			
+
 			// Map by SDK instance
 			map.set(this, self.IBehaviorInstance._GetInitInst().GetSdkInstance());
 		}
-		
+
 		// Example setter/getter property on script interface
-		set myProperty(n)
-		{
+		set myProperty(n) {
 			map.get(this)._SetMyProperty(n);
 		}
 
-		get myProperty()
-		{
+		get myProperty() {
 			return map.get(this)._GetMyProperty();
 		}
 	};
